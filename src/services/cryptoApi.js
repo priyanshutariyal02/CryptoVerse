98d002df858d231242ae6b9aa10a51cf9fc9c2f0
priyanshutariyal02 <tariyalpriyanshu6@gmail.com>
2024-02-02 21:27:54 +0530

    services folder which contains api files is set on api branch

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,17 @@
+// const options = {
+//     method: 'GET',
+//     url: 'https://coinranking1.p.rapidapi.com/coins',
+//     params: {
+//         referenceCurrencyUuid: 'yhjMzLPhuIDl',
+//         timePeriod: '24h',
+//         'tiers[0]': '1',
+//         orderBy: 'marketCap',
+//         orderDirection: 'desc',
+//         limit: '50',
+//         offset: '0'
+//     },
+//     headers: {
+//         'X-RapidAPI-Key': 'c8a611aaaemsh9ce160ac5587301p122890jsna31fcb774553',
+//         'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+//     }
+// };
